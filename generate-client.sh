5687ca8729cbe579e51c8d460d3abd44ea1f4321
Viacheslav Klimov <viacheslavklimov11@gmail.com>
2026-02-26 13:32:10 +0200

    Use openapi-normalizer option

--- generate-client.sh
+++ generate-client.sh
@@ -146,6 +146,7 @@ generate() {
     --global-property apiTests=false,modelTests=false \
     --schema-mappings  JsonNode="$JACKSON_JSON_NODE" \
     --import-mappings  JsonNode="$JACKSON_JSON_NODE" \
+    --openapi-normalizer SET_TAGS_FOR_ALL_OPERATIONS=Thingsboard \
     2>&1 | if [ "$VERBOSE" = true ]; then cat; else grep -v -e "^\[main\] INFO  o.o.codegen.*writing file" -e "^\[main\] INFO  o.o.c.languages.*Processing operation" -e "Unknown scheme.*loginPassword"; fi
 
   # Strip generated OpenAPI comment block and collapse multiple blank lines
